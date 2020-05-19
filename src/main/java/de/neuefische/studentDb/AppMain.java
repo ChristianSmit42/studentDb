0ee5085353259600c51e8365f32967ce0b3ad9af
Christian Smit <chrsmit@web.de>
2020-05-19 16:00:12 +0200

    CLEANED CODE

--- src/main/java/de/neuefische/studentDb/AppMain.java
+++ src/main/java/de/neuefische/studentDb/AppMain.java
@@ -11,34 +11,19 @@ public class AppMain {
 
         Student[] students = new Student[3];
         students[0] = new BiologyStudent("Paul", 1);
-        students[1] = new ChemistryStudent("Erhartdt", 2);
+        students[1] = new BiologyStudent("Erhartdt", 2);
         students[2] = new ChemistryStudent("Susanne", 3);
 
         StudentDb studentDb = new StudentDb(students);
 
-
-        //* USE OF list() *//*
-        for (Student student : studentDb.list()) {
-            System.out.println(student.toString());
-        }
-
-        //* USE OF StudentDb.toString *//*
-        System.out.println(studentDb.toString());
-
-        //* USE OF addStudent() *//*
-        studentDb.addStudent(13, "Egon");
-        System.out.println(studentDb.toString());
-
-        //* USE OF removeStudent() *//*
-        studentDb.removeStudent(3);
-        System.out.println(studentDb.toString());
-
         //* USE OF randomStudent()*//*
-        System.out.println(studentDb.randomStudent().toString());
-        System.out.println(studentDb.list()[1].toString() +
-                " has failed his classes: " +
-                studentDb.list()[1].hasFailed(2.5)
-        );
+        System.out.println("random Student: " +
+                studentDb.randomStudent().toString() +
+                "\n");
+
+        for(Student student: studentDb.list()){
+            System.out.printf("%s has passed: %s%n", student.toString(), student.hasPassed(1.55));
+        }
 
     }
 }
