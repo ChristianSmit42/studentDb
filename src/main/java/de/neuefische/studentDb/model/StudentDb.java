d98a236b06f507ce7f9dab424a491e6376d1b79e
Christian Smit <chrsmit@web.de>
2020-05-19 11:10:59 +0200

    FIX randomStudent possible NULLPOINTER

--- src/main/java/de/neuefische/studentDb/model/StudentDb.java
+++ src/main/java/de/neuefische/studentDb/model/StudentDb.java
@@ -19,45 +19,46 @@ public class StudentDb {
     private Student[] students;
 
     public StudentDb(Student[] students) {
+
         this.students = students;
     }
 
-    public Student[] list(){
+    public Student[] list() {
         return students;
     }
 
-    public Student randomStudent(){
-        int random = (int) ((Math.random()*students.length)-1);
+    public Student randomStudent() {
+        int random = (int) ((Math.random() * students.length);
         return students[random];
     }
 
-    public void removeStudent(int index){
-        Student[] tempstuds = new Student[this.students.length-1];
-        for(int i=0,k=0;i<this.students.length;i++){
-            if(i==index){
+    public void removeStudent(int index) {
+        Student[] tempstuds = new Student[this.students.length - 1];
+        for (int i = 0, k = 0; i < this.students.length; i++) {
+            if (i == index) {
                 continue;
             }
-            tempstuds[k++]=this.students[i];
+            tempstuds[k++] = this.students[i];
         }
         this.students = tempstuds;
     }
 
-    public void addStudent(int id, String name){
-        Student[] tempstuds = new Student[this.students.length+1];
+    public void addStudent(int id, String name) {
+        Student[] tempstuds = new Student[this.students.length + 1];
 
        /* for(int i=0;i<this.students.length;i++){
             tempstuds[i]=this.students[i];
         } */
         System.arraycopy(this.students, 0, tempstuds, 0, this.students.length);
 
-        tempstuds[this.students.length] = new Student(id,name);
+        tempstuds[this.students.length] = new Student(id, name);
         this.students = tempstuds;
     }
 
     @Override
     public String toString() {
-        StringBuilder allStudents= new StringBuilder();
-        for(Student student:students){
+        StringBuilder allStudents = new StringBuilder();
+        for (Student student : students) {
            /*
             allStudents+=student.toString() + "\n";
            */
