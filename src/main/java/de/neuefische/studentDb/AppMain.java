4dcdbeb0a55297698ff8ba19fb3ee24a7899c1d6
Christian Smit <chrsmit@web.de>
2020-05-22 08:30:08 +0200

    ADD Optional findByName

--- src/main/java/de/neuefische/studentDb/AppMain.java
+++ src/main/java/de/neuefische/studentDb/AppMain.java
@@ -6,11 +6,13 @@ import de.neuefische.studentDb.model.Student;
 import de.neuefische.studentDb.db.StudentDb;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class AppMain {
 
     public static void main(String[] args) {
             ArrayList<Student> students = new ArrayList<>();
+            Optional<Student> optionalStudent;
 
             students.add(new BiologyStudent("Birgit",1));
             students.add(new BiologyStudent("achsel",2));
@@ -22,11 +24,20 @@ public class AppMain {
             studentDb.addStudent(new ChemistryStudent("Greta",4));
             System.out.println(studentDb.toString());
 
-            studentDb.removeStudent(1);
+            studentDb.removeStudentByIndex(1);
             System.out.println(studentDb.toString());
 
-            studentDb.removeStudent(new BiologyStudent("Birgit" , 1));
+            studentDb.removeStudentByObject(new BiologyStudent("Birgit" , 1));
             System.out.println(studentDb.toString());
+
+            optionalStudent = studentDb.findStudentByName("Greta");
+            if(optionalStudent.isPresent()){
+                    System.out.println(optionalStudent.get().toString());
+            }else{
+                    System.out.println("Student not found #404");
+            }
+
+
 
 
 
